cf18c5d7376cef5cdefd52c279cafdbca637eebd
Rasmus Foged <rasmusf@plude.io>
2024-01-25 17:31:39 +0100

    Adding Naf.Io, adding core

---
+++ src/Naf.Core/Argument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Naf;
+
+public static class Argument
+{
+    /// <summary>
+    /// Defines that the <paramref name="value"/> is required.
+    /// </summary>
+    /// <param name="value">Nullable string.</param>
+    /// <param name="message">Message to pass to the ArgumentNullException.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static string NotNull(string? value, string message)
+    {
+        if (value is null)
+            throw new ArgumentNullException(message);
+
+        return value!;
+    }
+}
---
+++ src/Naf.IO/CsvReadSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Naf.IO;
+
+public class CsvReadSettings
+{
+    public bool SkipFirstLine = false;
+    public char FieldSplitter = ',';
+    public char TextQualifier = '"';
+
+    public CsvReadSettings()
+    {
+
+    }
+    public Encoding Encoding = System.Text.Encoding.UTF8;
+}
