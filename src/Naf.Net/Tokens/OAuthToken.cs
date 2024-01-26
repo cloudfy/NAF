85e37cb35011a8f03d9c969cdb10f242b13c8a2d
cloudfy <rasmus@foged.org>
2024-01-26 12:38:30 +0100

    Work changes

--- src/Naf.Net/Tokens/OAuthToken.cs
+++ src/Naf.Net/Tokens/OAuthToken.cs
@@ -4,18 +4,13 @@ namespace Naf.Net.Tokens;
 
 public class OAuth2Token : IAuthToken
 {
-    /// <summary></summary>
     [JsonProperty("refresh_token")]
     public string? RefreshToken { get; set; }
-    /// <summary></summary>
     [JsonProperty("access_token")]
     public string AccessToken { get; set; }
-    /// <summary></summary>
     [JsonProperty("expires_in")]
     public int? ExpiresIn { get; set; }
-    /// <summary></summary>
     [JsonProperty("token_type")]
     public string? TokenType { get; set; }
-    /// <summary></summary>
     public string? Scope { get; set; }
 }
